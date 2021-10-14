d6152627057f26a6dcf0b7c322af623f2c3a1a84
grgpratik <gurung.pratik04@gmail.com>
2021-10-14 15:11:31 +0545

    constant and theme of the app added

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,13 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-
-import 'domain/entities/app_error.dart';
-import 'domain/entities/movie_entity.dart';
-import 'domain/entities/no_params.dart';
-import 'domain/use_case/get_trending.dart';
+import 'package:flutter/services.dart';
 import 'package:pedantic/pedantic.dart';
 import 'di/get_it.dart' as getIt;
+import 'presentation/movie_app.dart';
 
-Future<void> main() async {
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  unawaited(
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]));
   unawaited(getIt.init());
-  GetTrending getTrending = getIt.getItInstance<GetTrending>();
-  final Either<AppError, List<MovieEntity>> eitherResponse =
-      await getTrending(NoParams());
-  eitherResponse.fold(
-    (l) {
-      print('error');
-      print(l);
-    },
-    (r) {
-      print('list of movies');
-      print(r);
-    },
-  );
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
-  }
+  runApp(MovieApp());
 }
---
+++ lib/presentation/movie_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieApp extends StatefulWidget {
+  const MovieApp({Key key}) : super(key: key);
+
+  @override
+  _MovieAppState createState() => _MovieAppState();
+}
+
+class _MovieAppState extends State<MovieApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
